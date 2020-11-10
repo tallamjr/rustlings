1ed5a2489959939afcc26fbf637b19bdc375a362
Tarek Allam <t.allam.jr@gmail.com>
2020-11-10 17:54:44 +0000

    Completed 'macros' and 'modules'
    	modified:   exercises/macros/macros1.rs
    	modified:   exercises/macros/macros2.rs
    	modified:   exercises/macros/macros3.rs
    	modified:   exercises/macros/macros4.rs
    	modified:   exercises/modules/modules1.rs
    	modified:   exercises/modules/modules2.rs
    	modified:   exercises/quiz4.rs

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,11 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($arg: expr) => {
+        format!("Hello {}", $arg);
+    };
+}
 
 #[cfg(test)]
 mod tests {
